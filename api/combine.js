4f36fd93f43ea16913677b8c1d546a90387f1a63
Peter Rosenthal <peter@rosenth.al>
2022-04-04 11:32:45 -0600

    first attempt at mvae within serverless function

---
+++ api/combine.js
@@ -0,0 +1,8 @@
+const mm_mvae = require('@magenta/music/node/music_vae');
+
+module.exports = (request, response) => {
+  const mvae = new mm_mvae.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_med_q2');
+  await mvae.initialize();
+  const { name = 'world' } = request.query;
+  response.status(200).send(`Hello ${name}! MVAE was able to init! I think?`);
+}
