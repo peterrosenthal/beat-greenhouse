f3b4ff2c54d4a4c54184d605be703893bd00ff24
Peter Rosenthal <peter@rosenth.al>
2022-04-04 11:36:41 -0600

    try transforming hello.js into node format

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,10 @@
+/*
 export default function handler(request, response) {
+  const { name = 'world' } = request.query;
+  response.status(200).send(`Hello ${name}!`);
+}
+*/
+module.exports = (request, response) => {
   const { name = 'world' } = request.query;
   response.status(200).send(`Hello ${name}!`);
 }
