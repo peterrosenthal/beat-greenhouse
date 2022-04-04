841b21e40b66798b82a818f0df8e99125185d0d2
Peter Rosenthal <peter@rosenth.al>
2022-04-04 17:41:23 -0600

    fix request name

--- api/combine.js
+++ api/combine.js
@@ -3,7 +3,7 @@ const mm = require('@magenta/music/node/music_vae');
 export default function handler(req, res) {
   const mvae = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_med_q2');
   mvae.initialize().then(() => {
-    const { name = 'world' } = request.query;
+    const { name = 'world' } = req.query;
     const responstring = JSON.stringify({
       message: `Hello ${name}! From an attempt at magenta shit!`,
     });
