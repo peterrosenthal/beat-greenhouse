a2d66d8a1dd8611ff1eb5146ef0c1d4666566d3b
Peter Rosenthal <peter@rosenth.al>
2022-04-04 15:05:20 -0600

    access serverless fns in main code with apiManager

--- api/combine.js
+++ api/combine.js
@@ -4,6 +4,9 @@ export default function handler(request, response) {
   const mvae = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_med_q2');
   mvae.initialize().then(() => {
     const { name = 'world' } = request.query;
-    response.status(200).send(`Hello ${name}! From an attempt at magenta shit!`);
+    const responstring = JSON.stringify({
+      message: `Hello ${name}! From an attempt at magenta shit!`,
+    });
+    response.status(200).send(JSON.stringify(responstring));
   });
 }
---
+++ src/managers/apiManager.ts
@@ -0,0 +1,35 @@
+const base = 'http://localhost:8080/api';
+
+export function init(): void {
+  get('/combine', new URLSearchParams({ name: 'peter' }))
+    .then((response: Record<string, unknown> | string) => {
+      if (response instanceof Object && response.message !== undefined) {
+        console.log(response.message);
+      } else {
+        console.log(response);
+      }
+    });
+}
+
+async function get(
+  path: string,
+  params?: URLSearchParams,
+): Promise<Record<string, unknown> | string> {
+  const paramstring = params instanceof URLSearchParams ? `?${params.toString()}` : '';
+  return fetch(
+    `${base}/${path}${paramstring}`,
+    { method: 'GET' },
+  ).then(function (response: Response) {
+    return response.text();
+  }).then(function(json: string) {
+    try {
+      const parsed = JSON.parse(json);
+      if (typeof parsed === 'string' && parsed.includes('{')) {
+        return JSON.parse(parsed);
+      }
+      return parsed;
+    } catch (e) {
+      return `couldn't parse string '${json}', error: ${e}`;
+    }
+  });
+}
