e5475c41372ae2614e18047d4d88ede21831ab4b
Peter Rosenthal <peter@rosenth.al>
2022-04-04 11:39:27 -0600

    slowly start adding mvae to the previously working hello.js

--- api/hello.js
+++ api/hello.js
@@ -4,6 +4,8 @@ export default function handler(request, response) {
   response.status(200).send(`Hello ${name}!`);
 }
 */
+const mm = require('@magenta/music/node/music_vae');
+
 module.exports = (request, response) => {
   const { name = 'world' } = request.query;
   response.status(200).send(`Hello ${name}!`);
