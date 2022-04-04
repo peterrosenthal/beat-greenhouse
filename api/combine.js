d23037e7058f9049a044a3c1766e33e16bd240d4
Peter Rosenthal <peter@rosenth.al>
2022-04-04 11:50:20 -0600

    try removing combine.js

--- api/combine.js
+++
@@ -1,8 +0,0 @@
-const mm_mvae = require('@magenta/music/node/music_vae');
-
-export default function handler(request, response) {
-  const mvae = new mm_mvae.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_med_q2);
-  await mvae.initialize();
-  const { name = 'world' } = request.query;
-  response.status(200).send(`Hello ${name}! MVAE was able to init! I think?`);
-}
